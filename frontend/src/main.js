9a66e89108e3096e33f58d9b531f6baf229b645e
myphan <tieumy.tozan@gmail.com>
2025-04-28 11:36:18 -0500

    fixed fontware

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router.js'  // Import the router
-import '@fortawesome/fontawesome-free/css/all.min.css'
+import './assets/fontawesome.css'
+
 
 
 createApp(App)
